33d1227db1149200e91911735c4256ef5e1edb0c
ControlVolume <onigiri282@gmail.com>
2024-03-19 20:10:30 +0900

    feat: reservation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.time.LocalDate;
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ReservationControl reservationControl = new ReservationControl();
+        reservationControl.makeReservation(1, 1,new Date());
+        reservationControl.confirmReservation(1);
+        reservationControl.cancelReservation(1);
+
     }
 }
