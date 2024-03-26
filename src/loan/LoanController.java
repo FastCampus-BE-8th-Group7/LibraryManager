4662b75a0d291b131c804b2c165e617b173b7dd2
EomHyeYeong <hy30051@naver.com>
2024-03-26 12:14:42 +0900

    feat: DB 연동하여 대출, 반납, 조회 기능을 구현

--- src/loan/LoanController.java
+++ src/loan/LoanController.java
@@ -1,16 +1,26 @@
 package loan;
 
+import book.Book;
+import book.BookTable;
+import user.User;
+import user.UserController;
+
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 
 public class LoanController {
     LoanTable loanTable = LoanTable.getInstance();
+    BookTable bookTable = BookTable.getInstance();
+    LoanDAO loanDAO = new LoanDAO();
+    UserController userController = new UserController();
 
     // checkBookLoanState
     public void checkBookLoanState(int bookId) {
         HashMap<Integer, Loan> loanMap = loanTable.loanMap;
         Collection<Loan> values = loanMap.values();
         boolean state = false;
+        Book book = BookTable.bookMap.get(bookId);
 
         for(Loan loan: values) {
             if(loan.getBookId()==bookId && loan.getReturnDate()!=null) {
@@ -20,47 +30,44 @@ public class LoanController {
         }
 
         if (state) {
-            System.out.println(bookId + "책은 대출중입니다.");
+            System.out.println(book.getTitle() + "책은 대출중입니다.");
         } else {
-            System.out.println(bookId + "책은 대출 가능합니다.");
+            System.out.println(book.getTitle() + "책은 대출 가능합니다.");
         }
     }
 
     // checkUserLoanState
     public void checkUserLoanState(int userId) {
-        HashMap<Integer, Loan> loanMap = loanTable.loanMap;
-        Collection<Loan> values = loanMap.values();
-        boolean state = false;
+        // user data 조회
+        User user = userController.getUser(userId);
 
+        System.out.printf("***** %s 님의 대출 목록 *****\n", user.getName());
+        System.out.printf("| %-30s | %-10s | %-10s | %-15s | %-10s |\n", "책 제목", "저자", "출판사", "ISBN", "출판일");
+        loanDAO.getLoanHistoryForUser(userId);
+        List<Book> books = loanDAO.getLoanHistoryForUser(userId);
 
-        for(Loan loan: values) {
-            if(loan.getUserId()==userId && loan.getReturnDate()==null) {
-                state = true;
-                break;
-            }
-        }
+        for (Book book : books) {
+            System.out.printf("| %-28s | %-10s | %-10s | %-15s | %-10s |\n",
+                    book.getTitle(), book.getAuthor(), book.getPublisher(), book.getIsbn(), book.getPublicationYear());
 
-        if (state) {
-            System.out.println(userId + "님은 현재 대출중입니다.");
-        } else {
-            System.out.println(userId + "님은 현재 대출 가능합니다.");
         }
     }
 
     // loanBook
     public void loanBook(int bookId, int userId) {
-        Loan newLoan = new Loan(bookId, userId);
-        loanTable.createTuple(newLoan);
+        User user = userController.getUser(userId);
+        String result = loanDAO.createLoanHistory(bookId, userId);
 
-        System.out.println(userId + "님이 책을 대출하였습니다.");
+        System.out.printf("-- %s 님의 대출 요청\n", user.getName());
+        System.out.println(result);
     }
 
     // returnBook
-    public void returnBook(int loanId) {
-        Loan returnLoan = loanTable.getLoan(loanId);
-        returnLoan.setReturnDate();
-        loanTable.loanMap.put(loanId, returnLoan);
+    public void returnBook(int userId) {
+        User user = userController.getUser(userId);
+        String result = loanDAO.updateLoanHistoryForReturn(userId);
 
-        System.out.println(returnLoan.getUserId() + "님이 책을 반납하였습니다.");
+        System.out.printf("-- %s 님의 반납 요청\n", user.getName());
+        System.out.println(result);
     }
 }
