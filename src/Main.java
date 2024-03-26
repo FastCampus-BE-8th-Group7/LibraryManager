5b167f32312fe59d85756b2461d9a622808ada32
bom04 <gil1122@naver.com>
2024-03-26 17:21:14 +0900

    chore: 개인 디비 정보를 .properties로 관리

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,47 @@
+import db.Database;
+import user.User;
+import user.UserController;
+import user.UserView;
 
-import reservation.ReservationControl;
-
-import java.util.Date;
+import java.io.IOException;
+import java.util.List;
 
 public class Main {
-    private static Connection con = null;
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        Database.connect();
+
+        UserController userController = new UserController();
+        UserView userView = new UserView();
+
+        // 사용자 등록
+        User user = userController.registerUser("손민주", "010-1234-5678", "sonminju@example.com", "12345");
+        System.out.println();
+
+        // 사용자 정보 수정
+        if (user != null) {
+            userController.updateUser(user.getUserId(), "손민주", "010-1233-5678", "sonminju@example.com", "12345");
+        }
+        System.out.println();
+
+        // 사용자 정보 조회
+        user = userController.getUser(user.getUserId());
+        if (user != null) {
+            userView.viewUserInfo(user);
+        }
+        System.out.println();
+
+        // 사용자 목록 조회
+        List<User> users = userController.getAllUsers();
+        userView.viewUserInfoList(users);
+        System.out.println();
+
+        // 사용자 삭제
+        if (user != null) {
+            userController.deleteUser(user.getUserId());
+        }
+        System.out.println();
+
+        Database.disconnect();
 
     }
 }
