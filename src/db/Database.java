acfc40985ac62685c3cdb302f75ae81b4ce865f3
EomHyeYeong <hy30051@naver.com>
2024-03-28 13:36:44 +0900

    fix: className config.properties에 포함

--- src/db/Database.java
+++ src/db/Database.java
@@ -11,6 +11,7 @@ public class Database {
 
     public static void connect() throws IOException {
         prop.load(new FileInputStream("config.properties"));
+        String classname = prop.getProperty("db.classname"); //"com.mysql.jdbc.Driver"
         String server = prop.getProperty("db.server"); // 서버 주소
         String database = prop.getProperty("db.database"); // 데이터베이스 이름
         String username = prop.getProperty("db.username"); // 접속자 id
@@ -18,8 +19,9 @@ public class Database {
 
         // JDBC 드라이버 로드
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
+            Class.forName(classname);
+        }
+        catch (ClassNotFoundException e) {
             System.err.println("JDBC 드라이버를 로드하는데에 문제 발생" + e.getMessage());
             e.printStackTrace();
             return;
